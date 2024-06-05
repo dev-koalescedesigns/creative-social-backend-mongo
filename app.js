ffdbf2fa0cd99051fef22a85397965e100783647
allisonmh <12721227+AllisonMH@users.noreply.github.com>
2024-06-05 13:25:07 -0400

    commit 2, modified app.js to create user database

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ async function run() {
   // Altas cluster specifics. Be sure it includes
   // a valid username and password! Note that in a production environment,
   // you do not want to store your password in plain-text here.
-  const uri =
-    "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority";
+  const uri = "mongodb+srv://allisonmh:nessa63@cluster0.an3khl6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
   // The MongoClient is the object that references the connection to our
   // datastore (Atlas, for example)
@@ -21,8 +20,8 @@ async function run() {
   // Provide the name of the database and collection you want to use.
   // If the database and/or collection do not exist, the driver and Atlas
   // will create them automatically when you first write data.
-  const dbName = "myDatabase";
-  const collectionName = "recipes";
+  const dbName = "userDatabase";
+  const collectionName = "users";
 
   // Create references to the database and collection in order to run
   // operations on them.
@@ -37,59 +36,31 @@ async function run() {
    * insert them all in one call with collection.insertMany().
    */
 
-  const recipes = [
+  const users = [
     {
-      name: "elotes",
-      ingredients: [
-        "corn",
-        "mayonnaise",
-        "cotija cheese",
-        "sour cream",
-        "lime",
-      ],
-      prepTimeInMinutes: 35,
+      firstName: "Anna",
+      lastName: "Smith",
+      email: "asmith@mail.com"
     },
     {
-      name: "loco moco",
-      ingredients: [
-        "ground beef",
-        "butter",
-        "onion",
-        "egg",
-        "bread bun",
-        "mushrooms",
-      ],
-      prepTimeInMinutes: 54,
+      firstName: "George",
+      lastName: "Lane",
+      email: "glane@mail.com"
     },
     {
-      name: "patatas bravas",
-      ingredients: [
-        "potato",
-        "tomato",
-        "olive oil",
-        "onion",
-        "garlic",
-        "paprika",
-      ],
-      prepTimeInMinutes: 80,
+      firstName: "Sylvia",
+      lastName: "Jones",
+      email: "sjones@mail.com"
     },
     {
-      name: "fried rice",
-      ingredients: [
-        "rice",
-        "soy sauce",
-        "egg",
-        "onion",
-        "pea",
-        "carrot",
-        "sesame oil",
-      ],
-      prepTimeInMinutes: 40,
+      firstName: "Kyle",
+      lastName: "Langley",
+      email: "klangley@mail.com"
     },
   ];
 
   try {
-    const insertManyResult = await collection.insertMany(recipes);
+    const insertManyResult = await collection.insertMany(users);
     console.log(`${insertManyResult.insertedCount} documents successfully inserted.\n`);
   } catch (err) {
     console.error(`Something went wrong trying to insert the new documents: ${err}\n`);
@@ -104,33 +75,33 @@ async function run() {
    * filters, and is used here to show its most basic use.
    */
 
-  const findQuery = { prepTimeInMinutes: { $lt: 45 } };
+  // const findQuery = { prepTimeInMinutes: { $lt: 45 } };
 
-  try {
-    const cursor = await collection.find(findQuery).sort({ name: 1 });
-    await cursor.forEach(recipe => {
-      console.log(`${recipe.name} has ${recipe.ingredients.length} ingredients and takes ${recipe.prepTimeInMinutes} minutes to make.`);
-    });
-    // add a linebreak
-    console.log();
-  } catch (err) {
-    console.error(`Something went wrong trying to find the documents: ${err}\n`);
-  }
+  // try {
+  //   const cursor = await collection.find(findQuery).sort({ name: 1 });
+  //   await cursor.forEach(recipe => {
+  //     console.log(`${recipe.name} has ${recipe.ingredients.length} ingredients and takes ${recipe.prepTimeInMinutes} minutes to make.`);
+  //   });
+  //   // add a linebreak
+  //   console.log();
+  // } catch (err) {
+  //   console.error(`Something went wrong trying to find the documents: ${err}\n`);
+  // }
 
   // We can also find a single document. Let's find the first document
   // that has the string "potato" in the ingredients list.
-  const findOneQuery = { ingredients: "potato" };
+  // const findOneQuery = { ingredients: "potato" };
 
-  try {
-    const findOneResult = await collection.findOne(findOneQuery);
-    if (findOneResult === null) {
-      console.log("Couldn't find any recipes that contain 'potato' as an ingredient.\n");
-    } else {
-      console.log(`Found a recipe with 'potato' as an ingredient:\n${JSON.stringify(findOneResult)}\n`);
-    }
-  } catch (err) {
-    console.error(`Something went wrong trying to find one document: ${err}\n`);
-  }
+  // try {
+  //   const findOneResult = await collection.findOne(findOneQuery);
+  //   if (findOneResult === null) {
+  //     console.log("Couldn't find any recipes that contain 'potato' as an ingredient.\n");
+  //   } else {
+  //     console.log(`Found a recipe with 'potato' as an ingredient:\n${JSON.stringify(findOneResult)}\n`);
+  //   }
+  // } catch (err) {
+  //   console.error(`Something went wrong trying to find one document: ${err}\n`);
+  // }
 
   /*
    * *** UPDATE A DOCUMENT ***
@@ -140,23 +111,24 @@ async function run() {
    * Here we update the PrepTimeInMinutes value on the document we
    * just found.
    */
-  const updateDoc = { $set: { prepTimeInMinutes: 72 } };
+  //const updateDoc = { $set: { prepTimeInMinutes: 72 } };
 
   // The following updateOptions document specifies that we want the *updated*
   // document to be returned. By default, we get the document as it was *before*
   // the update.
-  const updateOptions = { returnOriginal: false };
 
-  try {
-    const updateResult = await collection.findOneAndUpdate(
-      findOneQuery,
-      updateDoc,
-      updateOptions,
-    );
-    console.log(`Here is the updated document:\n${JSON.stringify(updateResult.value)}\n`);
-  } catch (err) {
-    console.error(`Something went wrong trying to update one document: ${err}\n`);
-  }
+  // const updateOptions = { returnOriginal: false };
+
+  // try {
+  //   const updateResult = await collection.findOneAndUpdate(
+  //     findOneQuery,
+  //     updateDoc,
+  //     updateOptions,
+  //   );
+  //   console.log(`Here is the updated document:\n${JSON.stringify(updateResult.value)}\n`);
+  // } catch (err) {
+  //   console.error(`Something went wrong trying to update one document: ${err}\n`);
+  // }
 
   /*      *** DELETE DOCUMENTS ***
    *
@@ -168,13 +140,13 @@ async function run() {
    */
 
 
-  const deleteQuery = { name: { $in: ["elotes", "fried rice"] } };
-  try {
-    const deleteResult = await collection.deleteMany(deleteQuery);
-    console.log(`Deleted ${deleteResult.deletedCount} documents\n`);
-  } catch (err) {
-    console.error(`Something went wrong trying to delete documents: ${err}\n`);
-  }
+  // const deleteQuery = { name: { $in: ["elotes", "fried rice"] } };
+  // try {
+  //   const deleteResult = await collection.deleteMany(deleteQuery);
+  //   console.log(`Deleted ${deleteResult.deletedCount} documents\n`);
+  // } catch (err) {
+  //   console.error(`Something went wrong trying to delete documents: ${err}\n`);
+  // }
 
   // Make sure to call close() on your client to perform cleanup operations
   await client.close();
